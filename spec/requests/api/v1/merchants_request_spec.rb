b520755b690ba848855608b776db9b417ccf7d2b
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-02 14:29:17 -0600

    deletes merchant functionality not required in spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -23,37 +23,4 @@ describe "Merchants API" do
     expect(response).to be_successful
     expect(merchant["id"]).to eq(id)
   end
-
-  it "can create a new merchant" do
-    merchant_params = {name: "KittenMerchant", description: "the cutest kittens we could find" }
-
-    post "/api/v1/merchants", params: {merchant: merchant_params}
-    merchant = Merchant.last
-
-    assert_response :success
-    expect(response).to be_successful
-    expect(merchant.name).to eq(merchant_params[:name])
-  end
-
-  it "can update an existing merchant" do
-    id = create(:merchant).id
-    previous_name = Merchant.last.name
-    merchant_params = {name: "PuppyMerchant"}
-
-    put "/api/v1/merchants/#{id}", params: {merchant: merchant_params}
-    merchant = Merchant.find_by(id: id)
-
-    expect(response).to be_successful
-    expect(merchant.name).to_not eq(previous_name)
-    expect(merchant.name).to eq("PuppyMerchant")
-  end
-
-  it "can destroy a merchant" do
-    merchant = create(:merchant)
-
-    expect{delete "/api/v1/merchants/#{merchant.id}"}.to change(Merchant, :count).by(-1)
-
-    expect(response).to be_successful
-    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
-  end
 end

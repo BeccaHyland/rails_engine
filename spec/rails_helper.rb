0b0fc707b6cd433741455dce22f6755429101bd5
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-02 15:20:29 -0600

    adds shoulda-matchers, adds model spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,7 @@ end
 RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
-  
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
@@ -24,4 +24,11 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end

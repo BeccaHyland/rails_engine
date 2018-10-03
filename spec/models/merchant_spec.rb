994d12a0f70e1ab1cee366c45b5a0ef93cf6e8fe
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-02 18:18:38 -0600

    adds invoice functionality for index and show

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ describe Merchant, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:name)}
   end
+
+  describe 'associations' do
+    it {should have_many(:invoices)}
+  end
 end

194ef4924efe996b66e9096478ba48c5830c7714
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-04 17:56:58 -0600

    saves and comments alternate test spec for later reference if needed

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -14,34 +14,6 @@ describe Merchant, type: :model do
   describe 'class methods' do
     before (:each) do
 
-      # customer = Customer.create(first_name: "Firstname", last_name: "Lastname")
-      # #Merchant 1
-      # @merchant1 = Merchant.create(name: "AwesomeMerchant")
-      # item1 = Item.create(name: "AwesomeItem", description: "It's awesome", unit_price: 10, merchant_id: @merchant1.id)
-      # invoice1 = Invoice.create(customer_id: customer.id, merchant_id: @merchant1.id, status: "shipped")
-      # invoice_item1 = InvoiceItem.create(item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 10)
-      # transaction1 = Transaction.create(invoice_id: invoice1.id, credit_card_number: '1234567890', result: 'success')
-      #
-      # #Merchant 2
-      # @merchant2 = Merchant.create(name: "GreatMerchant")
-      # item2 = Item.create(name: "GreatItem", description: "It's great", unit_price: 20, merchant_id: @merchant2.id)
-      # invoice2 = Invoice.create(customer_id: customer.id, merchant_id: @merchant2.id, status: "shipped")
-      # invoice_item2 = InvoiceItem.create(item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 20)
-      # transaction2 = Transaction.create(invoice_id: invoice2.id, credit_card_number: '1234567890', result: 'success')
-      #
-      # #Merchant 3
-      # @merchant3 = Merchant.create(name: "GoodMerchant")
-      # item3 = Item.create(name: "GoodItem", description: "It's good", unit_price: 30, merchant_id: @merchant3.id)
-      #
-      # invoice3 = Invoice.create(customer_id: customer.id, merchant_id: @merchant3.id, status: "shipped")
-      # invoice_item3 = InvoiceItem.create(item_id: item3.id, invoice_id: invoice3.id, quantity: 3, unit_price: 30)
-      # transaction3 = Transaction.create(invoice_id: invoice3.id, credit_card_number: '1234567890', result: 'failed')
-      # transaction4 = Transaction.create(invoice_id: invoice3.id, credit_card_number: '1234567890', result: 'success')
-      #
-      # invoice4 = Invoice.create(customer_id: customer.id, merchant_id: @merchant3.id, status: "shipped")
-      # invoice_item4 = InvoiceItem.create(item_id: item3.id, invoice_id: invoice4.id, quantity: 4, unit_price: 40)
-      # transaction5 = Transaction.create(invoice_id: invoice4.id, credit_card_number: '1234567890', result: 'failed')
-
       @merchant1, @merchant2, @merchant3 = create_list(:merchant, 3)
       @invoice1 = create(:invoice, merchant: @merchant1)
       @invoice2 = create(:invoice, merchant: @merchant1)
@@ -74,3 +46,34 @@ describe Merchant, type: :model do
     end
   end
 end
+
+#regular RSPEC version of same before(:each) test setup as above
+#save for reference
+
+# customer = Customer.create(first_name: "Firstname", last_name: "Lastname")
+# #Merchant 1
+# @merchant1 = Merchant.create(name: "AwesomeMerchant")
+# item1 = Item.create(name: "AwesomeItem", description: "It's awesome", unit_price: 10, merchant_id: @merchant1.id)
+# invoice1 = Invoice.create(customer_id: customer.id, merchant_id: @merchant1.id, status: "shipped")
+# invoice_item1 = InvoiceItem.create(item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 10)
+# transaction1 = Transaction.create(invoice_id: invoice1.id, credit_card_number: '1234567890', result: 'success')
+#
+# #Merchant 2
+# @merchant2 = Merchant.create(name: "GreatMerchant")
+# item2 = Item.create(name: "GreatItem", description: "It's great", unit_price: 20, merchant_id: @merchant2.id)
+# invoice2 = Invoice.create(customer_id: customer.id, merchant_id: @merchant2.id, status: "shipped")
+# invoice_item2 = InvoiceItem.create(item_id: item2.id, invoice_id: invoice2.id, quantity: 2, unit_price: 20)
+# transaction2 = Transaction.create(invoice_id: invoice2.id, credit_card_number: '1234567890', result: 'success')
+#
+# #Merchant 3
+# @merchant3 = Merchant.create(name: "GoodMerchant")
+# item3 = Item.create(name: "GoodItem", description: "It's good", unit_price: 30, merchant_id: @merchant3.id)
+#
+# invoice3 = Invoice.create(customer_id: customer.id, merchant_id: @merchant3.id, status: "shipped")
+# invoice_item3 = InvoiceItem.create(item_id: item3.id, invoice_id: invoice3.id, quantity: 3, unit_price: 30)
+# transaction3 = Transaction.create(invoice_id: invoice3.id, credit_card_number: '1234567890', result: 'failed')
+# transaction4 = Transaction.create(invoice_id: invoice3.id, credit_card_number: '1234567890', result: 'success')
+#
+# invoice4 = Invoice.create(customer_id: customer.id, merchant_id: @merchant3.id, status: "shipped")
+# invoice_item4 = InvoiceItem.create(item_id: item3.id, invoice_id: invoice4.id, quantity: 4, unit_price: 40)
+# transaction5 = Transaction.create(invoice_id: invoice4.id, credit_card_number: '1234567890', result: 'failed')

df2e2efc61add4f93d09e8d2447c236028f9f9ac
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-06 12:20:18 -0600

    merchant find and find_all by attributes passing spec harness

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get 'most_revenue', to: 'most_revenue#index'
-        get 'find', to: 'find#show'
+        get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
       end
       resources :merchants, only: [:index, :show]
--- spec/requests/api/v1/merchants/search_request_spec.rb
+++ spec/requests/api/v1/merchants/search_request_spec.rb
@@ -1,21 +1,34 @@
 require 'rails_helper'
 
-describe 'merchant search API' do~
-  before (:each) do
-    @merchant1 = Merchant.create(name: "AwesomeMerchant")
-    @merchant2 = Merchant.create(name: "GreatMerchant")
-    @merchant3 = Merchant.create(name: "GoodMerchant")
-  end
-
+describe 'merchant search API' do
   it 'returns one merchant by name' do
+    merchant1 = create(:merchant, name: "AwesomeMerchant")
+    merchant2 = create(:merchant, name: "GreatMerchant")
+    merchant3 = create(:merchant, name: "GoodMerchant")
+
     search_params = "AwesomeMerchant"
-    get "api/v1/merchants/find?name=#{search_params}"
+    get "/api/v1/merchants/find?name=#{search_params}"
 
     expect(response).to be_successful
 
     found_merchant = JSON.parse(response.body)
 
-    expect(found_merchant.id).to eq(@merchant1.id)
+    expect(found_merchant["id"]).to eq(merchant1.id)
+  end
+
+  it 'returns all merchants by date' do
+    merchant1, merchant2 = create_list(:merchant, 2, created_at: '2018-09-09 00:00:00')
+    merchant3 = create(:merchant, created_at: '2018-10-10 00:00:00')
+
+    get "/api/v1/merchants/find_all?created_at=#{merchant1.created_at}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(response.body).to include("#{merchant1.id}")
+    expect(response.body).to include("#{merchant2.id}")
+    expect(response.body).to_not include("#{merchant3.id}")
   end
 
 end

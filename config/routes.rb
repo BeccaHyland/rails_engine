6b60b119836ab2db2b763d5ac6e0fac28297c9ab
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-07 13:06:18 -0600

    adds merchant items endpoint functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get 'most_revenue', to: 'most_revenue#index'
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
+        get ':id/items', to: 'items#index'
+        get 'most_revenue', to: 'most_revenue#index'
         get 'random', to: 'random#show'
       end
       resources :merchants, only: [:index, :show]
+
       resources :items, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
